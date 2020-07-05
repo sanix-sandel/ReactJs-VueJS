18f749ba276fc0e8e8d5acd51d565b1bdc2d5455
sanix sandel <sanicksikani@gmail.com>
2020-07-05 14:12:11 +0300

    active link

--- Documents/support-center/src/components/NavMenu.vue
+++ Documents/support-center/src/components/NavMenu.vue
@@ -1,7 +1,15 @@
 <template>
     <nav class="menu">
-        <router-link :to="{name:'home'}">Home</router-link>
-        <router-link :to="{name:'faq'}">FAQ</router-link>
+        <router-link :to="{name:'home'}" exact>Home</router-link>
+        <router-link :to="{name:'faq'}" exact>FAQ</router-link>
        
     </nav>
 </template>
+
+<style lang="stylus" scoped>
+@import '../style/imports';
+
+.router-link-active {
+border-bottom-color: $primary-color;
+}
+</style>
